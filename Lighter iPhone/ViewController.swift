15c0e09f87beed731167e6b4cce72d71a3593946
holiev <artur.golev.98@gmail.com>
2022-08-01 15:46:18 +0300

    Implemented background color change

--- Lighter iPhone/ViewController.swift
+++ Lighter iPhone/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = false
+    
+    @IBAction func ButtonPressed(_ sender: UIButton) {
+        lightOn = !lightOn
+      
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+        }
+        else {
+            view.backgroundColor = .black
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
 
